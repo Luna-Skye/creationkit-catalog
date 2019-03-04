11342d0e498f499414645615d14be0688b25da02
Andy <sepshuncontact@gmail.com>
2019-03-03 19:22:48 -0500

    Text Input
    Added a local text input component, currently using for the filter in the results panel

--- src/components/CatalogResults.vue
+++ src/components/CatalogResults.vue
@@ -1,15 +1,23 @@
 <template>
 <div class="catalog-results">
-	<div class="header">
+	<div class="header -grid">
         <p class="title">RESULTS</p>
+        <TextInput placeholder="FILTER" v-model="filter" edge="left" />
     </div>
     <p class="counter">222 Results</p>
+	
 </div>
 </template>
 
 <script>
+import TextInput from '@/components/TextInput.vue'
 export default {
-    
+    components: { TextInput },
+    data() {
+        return {
+            filter: ''
+        }
+    }
 }
 </script>
 
@@ -18,6 +26,12 @@ export default {
     background: rgba($color-black, 0.8);
     border-left: 2px solid $color-white;
     border-right: 2px solid $color-white;
+    
+    .header.-grid {
+        display: grid;
+        grid-template-columns: auto 260px;
+        align-items: center;
+    }
     
     .counter {
         color: rgba($color-white, 0.8);
---
+++ src/components/TextInput.vue
@@ -0,0 +1,65 @@
+<template>
+<div class="input" :class="[`-${edge}edge`]">
+    <EdgeFlare
+        v-if="edge === 'left' || edge === 'both'"
+        class="-left"
+    />
+    <EdgeFlare
+        v-if="edge === 'right' || edge === 'both'"
+        class="-right"
+    />
+    
+	<input
+        type="text"
+        :placeholder="placeholder"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+    >
+</div>
+</template>
+
+<script>
+export default {
+    props: {
+        value: {
+            type: String,
+            default: ''
+        },
+        placeholder: {
+            type: String,
+            default: ''
+        },
+        edge: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.input {
+    position: relative;
+    
+    &.-leftedge { padding-left: 34px; }
+    &.-rightedge { padding-right: 34px; }
+    &.-bothedge { padding: 0 34px; }
+    
+    .edge-flare { top: -6px; }
+    .edge-flare.-left { left: 0; }
+    .edge-flare.-right { right: 0; transform: rotate(180deg); }
+    
+	input {
+        background: rgba($color-black, 0.8);
+        border: none;
+        color: $color-white;
+        font-size: 18px;
+        padding: 8px 24px;
+        border-bottom: 2px solid $color-white;
+        border-top: 2px solid $color-white;
+        width: calc(100% - 48px);
+        
+        &:focus { outline: none; }
+    }
+}
+</style>
